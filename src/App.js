0f16b23c631ca58393b8e7576fec5513ad44ee11
billyh0217 <billy.han@terabyte.co.nz>
2022-10-26 23:53:29 +1300

    install react query, axios and scss and update FE style

--- src/App.js
+++ src/App.js
@@ -1,11 +1,27 @@
-import './App.css';
-
+import './App.scss';
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import Home from './Pages/Home';
 function App() {
-  return (
-    <div className="App">
-      
-    </div>
-  );
+
+	const client = new QueryClient({
+		defaultOptions: {
+			queries: {
+				refetchOnWindowFocus: false,
+			},
+		},
+	});
+
+	return (
+		<div className="App">
+			<QueryClientProvider client={client}>
+				<div className="container">
+					<div className="row">
+						<Home />
+					</div>
+				</div>
+			</QueryClientProvider>
+		</div>
+	);
 }
 
 export default App;
